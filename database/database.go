8557b2f9d777be4d9b4213f289920f2f92b7c91e
Your Name <roihansori12@gmail.com>
2022-10-23 18:14:25 +0700

    Fix database

--- database/database.go
+++ database/database.go
@@ -18,7 +18,7 @@ var (
 	DB_NAME     string = os.Getenv("DB_NAME")     //util.GetConfig("DB_NAME")
 	DB_HOST     string = os.Getenv("DB_HOST")     //util.GetConfig("DB_HOST")
 	DB_PORT     string = os.Getenv("DB_PORT")     //util.GetConfig("DB_PORT")
-	DB_SSLMODE  string = os.Getenv("DB_SSLMODE")  //util.GetConfig("DB_PORT")
+	// DB_SSLMODE  string = os.Getenv("DB_SSLMODE")  //util.GetConfig("DB_PORT")
 )
 
 func Connect() {
@@ -26,13 +26,12 @@ func Connect() {
 
 	// dsn := "host=localhost user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai"
 
-	var dsn string = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
+	var dsn string = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s TimeZone=Asia/Shanghai",
 		DB_HOST,
 		DB_USERNAME,
 		DB_PASSWORD,
 		DB_NAME,
 		DB_PORT,
-		DB_SSLMODE,
 	)
 
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
